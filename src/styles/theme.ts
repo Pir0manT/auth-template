48b8adab7d19a89ebb36c50e61fae3623ce88de1
Andrey Sergeyev <savilit@yandex.ru>
2024-04-19 04:34:26 +0300

    feat: Начальные установки темы и шрифта

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+'use client'
+import { createTheme } from '@mui/material/styles'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const theme = createTheme({
+  typography: {
+    fontFamily: poppins.style.fontFamily,
+  },
+})
+
+export default theme
