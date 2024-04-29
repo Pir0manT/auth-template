a5d669b48fa78f2f7fc0ad70cff6c44ce8f4edbe
Andrey Sergeyev <savilit@yandex.ru>
2024-04-29 14:10:57 +0300

    hotfix: подготовка к deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
- // output: 'standalone',
+  //output: 'standalone',
   experimental: {
     optimizePackageImports: ['@mui/lab','@mui/icons-material'],
   },
